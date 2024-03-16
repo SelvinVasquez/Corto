feb7b24106d29faf81b31381e5739054424ea85d
SelvinVasquez <svasquezl9@miumg.edu.gt>
2024-03-16 10:37:17 -0600

    Selvin Vasquez 0909-23-304 Jose Carlos Pirir 0909-22-8903 Corto

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int size;
+
+    cout << "Ingrese la cantidad de numeros: ";
+    cin >> size;
+
+    int numeros[size];
+
+    for (int i = 0; i < size; i++) {
+        cout << "Ingrese el numero " << i+1 << ": ";
+        cin >> numeros[i];
+    }
+
+    for (int i = 0; i < size-1; i++) {
+        for (int j = 0; j < size-i-1; j++) {
+            if (numeros[j] > numeros[j+1]) {
+                int temp = numeros[j];
+                numeros[j] = numeros[j+1];
+                numeros[j+1] = temp;
+            }
+        }
+    }
+
+    cout << "Numeros ordenados de menor a mayor: ";
+    for (int i = 0; i < size; i++) {
+        cout << numeros[i] << " ";
+    }
+
+    return 0;
+}
